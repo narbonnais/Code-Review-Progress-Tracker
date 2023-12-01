a3ec20f4f19217c160b80c5aefe8957e7e9ea728
Alexis Fabre <25547857+narbonnais@users.noreply.github.com>
2023-12-01 20:01:59 +0900

    Refactor config and commands

--- src/config.ts
+++ src/config.ts
@@ -2,14 +2,19 @@ import * as vscode from 'vscode';
 
 export class Config {
     private context: vscode.ExtensionContext;
+    private decorationTypes: Map<string, vscode.TextEditorDecorationType> = new Map();
 
-    private darkIcons: Map<string, string> = new Map();
-    private lightIcons: Map<string, string> = new Map();
-    private colorsCodes: Map<string, string> = new Map();
-    private rulerColorCodes: Map<string, string> = new Map();
-    private decorationRenderOptions: Map<string, vscode.DecorationRenderOptions> = new Map();
+    private static readonly COLORS = {
+        green: { code: '#4caf500a', ruler: '#4caf50', darkIcon: 'green-dark.svg', lightIcon: 'green-light.svg' },
+        red: { code: '#f443360a', ruler: '#f44336', darkIcon: 'red-dark.svg', lightIcon: 'red-light.svg' },
+        yellow: { code: '#ffeb3b0a', ruler: '#ffeb3b', darkIcon: 'yellow-dark.svg', lightIcon: 'yellow-light.svg' }
+    };
 
-    public decorationTypes: Map<string, vscode.TextEditorDecorationType> = new Map();
+    private static readonly CODE_TO_COLOR = {
+        'ok': 'green',
+        'warning': 'yellow',
+        'danger': 'red'
+    };
 
     constructor(context: vscode.ExtensionContext) {
         this.context = context;
@@ -22,48 +27,32 @@ export class Config {
     private getIconPath(iconName: string): string {
         return this.context.asAbsolutePath(`images/${iconName}`);
     }
-    
-    private makeDecorationRenderOptionsByColor(color: string): vscode.DecorationRenderOptions {
-        const decoration: vscode.DecorationRenderOptions = {
+
+    private makeDecorationRenderOptions(colorKey: string): vscode.DecorationRenderOptions {
+        const colorConfig = Config.COLORS[colorKey as keyof typeof Config.COLORS];
+        return {
             light: {
-                backgroundColor: this.colorsCodes.get(color) as string,
-                gutterIconPath: this.getIconPath(this.lightIcons.get(color) as string),
-                overviewRulerColor: this.rulerColorCodes.get(color) as string,
+                backgroundColor: colorConfig.code,
+                gutterIconPath: this.getIconPath(colorConfig.lightIcon),
+                overviewRulerColor: colorConfig.ruler,
             },
             dark: {
-                backgroundColor: this.colorsCodes.get(color) as string,
-                gutterIconPath: this.getIconPath(this.darkIcons.get(color) as string),
-                overviewRulerColor: this.rulerColorCodes.get(color) as string,
+                backgroundColor: colorConfig.code,
+                gutterIconPath: this.getIconPath(colorConfig.darkIcon),
+                overviewRulerColor: colorConfig.ruler,
             },
             isWholeLine: true,        
         };
-        return decoration;
     }
 
     private setup(): void {
-        
-        this.darkIcons.set('green', 'green-dark.svg');
-        this.darkIcons.set('red', 'red-dark.svg');
-        this.darkIcons.set('yellow', 'yellow-dark.svg');
+        Object.keys(Config.COLORS).forEach(colorKey => {
+            const decorationOptions = this.makeDecorationRenderOptions(colorKey);
+            this.decorationTypes.set(colorKey, vscode.window.createTextEditorDecorationType(decorationOptions));
+        });
+    }
 
-        this.lightIcons.set('green', 'green-light.svg');
-        this.lightIcons.set('red', 'red-light.svg');
-        this.lightIcons.set('yellow', 'yellow-light.svg');
-
-        this.colorsCodes.set('green', '#4caf500a');
-        this.colorsCodes.set('red', '#f443360a');
-        this.colorsCodes.set('yellow', '#ffeb3b0a');
-
-        this.rulerColorCodes.set('green', '#4caf50');
-        this.rulerColorCodes.set('red', '#f44336');
-        this.rulerColorCodes.set('yellow', '#ffeb3b');
-
-        this.decorationRenderOptions.set('green', this.makeDecorationRenderOptionsByColor('green'));
-        this.decorationRenderOptions.set('red', this.makeDecorationRenderOptionsByColor('red'));
-        this.decorationRenderOptions.set('yellow', this.makeDecorationRenderOptionsByColor('yellow'));
-
-        this.decorationTypes.set('green', vscode.window.createTextEditorDecorationType(this.decorationRenderOptions.get('green') as vscode.DecorationRenderOptions));
-        this.decorationTypes.set('red', vscode.window.createTextEditorDecorationType(this.decorationRenderOptions.get('red') as vscode.DecorationRenderOptions));
-        this.decorationTypes.set('yellow', vscode.window.createTextEditorDecorationType(this.decorationRenderOptions.get('yellow') as vscode.DecorationRenderOptions));
+    public getDecorationType(colorKey: string): vscode.TextEditorDecorationType | undefined {
+        return this.decorationTypes.get(Config.CODE_TO_COLOR[colorKey as keyof typeof Config.CODE_TO_COLOR]);
     }
 };
--- src/extension.ts
+++ src/extension.ts
@@ -1,164 +1,73 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { Config } from './config';
 import { State } from './state';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "code-review-progress-tracker" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-review-progress-tracker" is now active!');
+    const config = new Config(context);
+    const state = new State();
 
-	const config = new Config(context);
-	const state = new State();
+    let temp = context.workspaceState.get("State");
+    if (temp) {
+        state.loadFromJson(temp);
+    }
 
-	let temp = context.workspaceState.get("State");
-	if (temp) {
-		state.loadFromJson(temp);
-	}
+    const updateDecorations = (activeEditor: vscode.TextEditor) => {
+        if (!activeEditor) return;
+        const path = activeEditor.document.uri.path.toString();
+        ['ok', 'warning', 'danger'].forEach(status => {
+            const decorationType = config.getDecorationType(status);
+            const ranges = state.getRanges(status as 'ok' | 'warning' | 'danger', path);
+            if (decorationType) {
+                activeEditor.setDecorations(decorationType, ranges || []);
+            }
+        });
+        context.workspaceState.update("State", state.toJson());
+    }
 
-	// Update files when vscode is opened.
-	let updateDecorations = (activeEditor: vscode.TextEditor) => {
-		if (!activeEditor) {
-			return;
-		}
-		const path = activeEditor.document.uri.path.toString();
-		const greenDecorationType = config.decorationTypes.get('green') as vscode.TextEditorDecorationType;
-		const yellowDecorationType = config.decorationTypes.get('yellow') as vscode.TextEditorDecorationType;
-		const redDecorationType = config.decorationTypes.get('red') as vscode.TextEditorDecorationType;
-		const ranges_ok = state.getRanges("ok", path);
-		const ranges_warning = state.getRanges("warning", path);
-		const ranges_danger = state.getRanges("danger", path);
-		if (ranges_ok) {
-			activeEditor.setDecorations(greenDecorationType, ranges_ok);
-		} else {
-			activeEditor.setDecorations(greenDecorationType, []);
-		}
-		if (ranges_warning) {
-			activeEditor.setDecorations(yellowDecorationType, ranges_warning);
-		} else {
-			activeEditor.setDecorations(yellowDecorationType, []);
-		}
-		if (ranges_danger) {
-			activeEditor.setDecorations(redDecorationType, ranges_danger);
-		} else {
-			activeEditor.setDecorations(redDecorationType, []);
-		}
-		context.workspaceState.update("State", state.toJson());
-	}
-		// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let reviewOkDisposable = vscode.commands.registerCommand('code-review-progress-tracker.reviewedOk', () => {
-		// The code you place here will be executed every time your command is executed
+    const handleReviewCommand = (type: 'ok' | 'warning' | 'danger' | 'clear') => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) return;
 
-		// Set gutter decorations
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			return;
-		}
-		const path = editor.document.uri.path.toString();
-		const firstLine = editor.selection.active;
-		const lastLine = editor.selection.anchor;
-		const textRange = new vscode.Range(firstLine, lastLine);
-		state.removeRangeFromAllTypesInFile(path, textRange);
-		state.addRange("ok", path, textRange);
-		updateDecorations(editor);
-	});
+        const path = editor.document.uri.path.toString();
+        const textRange = new vscode.Range(editor.selection.start, editor.selection.end);
 
-	let reviewWarningDisposable = vscode.commands.registerCommand('code-review-progress-tracker.reviewedWarning', () => {
-		// The code you place here will be executed every time your command is executed
+        if (type !== 'clear') {
+            state.removeRangeFromAllTypesInFile(path, textRange);
+            state.addRange(type, path, textRange);
+        } else {
+            state.removeRangeFromAllTypesInFile(path, textRange);
+        }
 
-		// Set gutter decorations
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			return;
-		}
-		const path = editor.document.uri.path.toString();
-		const firstLine = editor.selection.active;
-		const lastLine = editor.selection.anchor;
-		const textRange = new vscode.Range(firstLine, lastLine);
-		state.removeRangeFromAllTypesInFile(path, textRange);
-		state.addRange("warning", path, textRange);
-		updateDecorations(editor);
-	});
+        updateDecorations(editor);
+    }
 
-	let reviewDangerDisposable = vscode.commands.registerCommand('code-review-progress-tracker.reviewedDanger', () => {
-		// The code you place here will be executed every time your command is executed
+    const registerReviewCommand = (type: 'ok' | 'warning' | 'danger' | 'clear') => {
+        let commandId = `code-review-progress-tracker.reviewed${type.charAt(0).toUpperCase() + type.slice(1)}`;
+        return vscode.commands.registerCommand(commandId, () => handleReviewCommand(type));
+    }
 
-		// Set gutter decorations
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			return;
-		}
-		const path = editor.document.uri.path.toString();
-		const firstLine = editor.selection.active;
-		const lastLine = editor.selection.anchor;
-		const textRange = new vscode.Range(firstLine, lastLine);
-		state.removeRangeFromAllTypesInFile(path, textRange);
-		state.addRange("danger", path, textRange);
-		updateDecorations(editor);
-	});
-
-	let reviewClearDisposable = vscode.commands.registerCommand('code-review-progress-tracker.reviewedClear', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Set gutter decorations
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			return;
-		}
-		const path = editor.document.uri.path.toString();
-		const firstLine = editor.selection.active;
-		const lastLine = editor.selection.anchor;
-		const textRange = new vscode.Range(firstLine, lastLine);
-		state.removeRangeFromAllTypesInFile(path, textRange);
-		updateDecorations(editor);
-	});
-
-	let reviewClearAllDisposable = vscode.commands.registerCommand('code-review-progress-tracker.reviewedClearAll', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Set gutter decorations
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			return;
-		}
-		const path = editor.document.uri.path.toString();
-		state.clearFile(path);
-		updateDecorations(editor);
-	});
-
-	let activeEditor: vscode.TextEditor | undefined = undefined;
-	activeEditor = vscode.window.activeTextEditor!;
-	updateDecorations(activeEditor);
-	// When editor switches update activeEditor and update decorations.
-	vscode.window.onDidChangeActiveTextEditor(editor => {
-		activeEditor = editor;
-		if (editor) {
-			updateDecorations(editor);
-		}
-	}, null, context.subscriptions);
-
-	// If file name changes update key for markedEditors
-	vscode.workspace.onWillRenameFiles((event) => {
-		event.files.forEach((files) => {
-			const oldPath = files.oldUri.path.toString();
-			const newPath = files.newUri.path.toString();
-			state.changeFilename(oldPath, newPath);
-		});
-	}, null, context.subscriptions);
-
-
-	context.subscriptions.push(reviewOkDisposable);
-	context.subscriptions.push(reviewWarningDisposable);
-	context.subscriptions.push(reviewDangerDisposable);
-	context.subscriptions.push(reviewClearDisposable);
-	context.subscriptions.push(reviewClearAllDisposable);
+    context.subscriptions.push(
+        registerReviewCommand('ok'),
+        registerReviewCommand('warning'),
+        registerReviewCommand('danger'),
+        registerReviewCommand('clear'),
+        vscode.commands.registerCommand('code-review-progress-tracker.reviewedClearAll', () => {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) return;
+            state.clearFile(editor.document.uri.path.toString());
+            updateDecorations(editor);
+        }),
+        vscode.window.onDidChangeActiveTextEditor(editor => {
+            if (editor) updateDecorations(editor);
+        }),
+        vscode.workspace.onWillRenameFiles(event => {
+            event.files.forEach(file => {
+                state.changeFilename(file.oldUri.path.toString(), file.newUri.path.toString());
+            });
+        })
+    );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
